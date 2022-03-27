b550419cc533e41c91e0297a15262f08cca16b0c
lisareichelson <lisareichelson@ufl.edu>
2022-03-27 15:13:01 -0400

    generic line graph created

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,49 +1,48 @@
 import React, { Component } from "react";
 import Chart from "chart.js/auto";
 
+//Below is a default 2 variable line graph. I have no idea how to resize it.
 export default class LineChart extends Component {
   chartRef = React.createRef();
-
   componentDidMount() {
     const ctx = this.chartRef.current.getContext("2d");
-
     new Chart(ctx, {
       type: "line",
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: "Y Axis(units) over X Axis (Units)",
+          },
+        },
+      },
       data: {
-        labels: [
-          "Sunday",
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday",
-        ],
+        labels: [0, 20, 40, 60, 80, 100, 140], //X Axis Values
         datasets: [
           {
-            data: [86, 114, 106, 106, 107, 111, 133],
-            label: "Total",
+            data: [86, 114, 106, 106, 107, 111, 133], //Y Axis Values
+            label: "Country 1",
             borderColor: "#3e95cd",
             backgroundColor: "#7bb6dd",
             //fill: False,
           },
           {
             data: [70, 90, 44, 60, 83, 90, 100],
-            label: "Accepted",
+            label: "Country 2",
             borderColor: "#3cba9f",
             backgroundColor: "#71d1bd",
             //fill: False,
           },
           {
             data: [10, 21, 60, 44, 17, 21, 17],
-            label: "Pending",
+            label: "Country 3",
             borderColor: "#ffa500",
             backgroundColor: "#ffc04d",
             //fill: False,
           },
           {
             data: [6, 3, 2, 2, 7, 0, 16],
-            label: "Rejected",
+            label: "Country 4",
             borderColor: "#c45850",
             backgroundColor: "#d78f89",
             // fill: False,
