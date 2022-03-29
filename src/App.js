417e97532225e1d22b392364f8fd007e462783f2
lisareichelson <lisareichelson@ufl.edu>
2022-03-29 14:21:28 -0400

    added react-dropdown package and basic dropdown selection logic.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import LineChart from "./components/chart.js";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 import logo from "./logo.svg";
 import wine from "./wine.gif";
 import "./App.css";
 import React, { Component } from "react";
 import { render } from "@testing-library/react";
+
+const countries = ["A", "B", "C"]; //Change to load in all the country datas we have
+const defaultCountry = countries[0]; //Change to whatever the U.S. is in the list
 
 class App extends Component {
   constructor() {
@@ -19,6 +24,23 @@ class App extends Component {
         <div className="App-header">
           <img src={wine} width="150" height="150" alt="wine" />
           <h2>Welcome to WineAlyze</h2>
+        </div>
+        Select a Country to begin.
+        <div
+          // How can I center this? I resized it but thats all ive got.
+          style={{
+            display: "flex",
+            width: "300px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Dropdown
+            options={countries}
+            onChange={this._onSelect}
+            value={defaultCountry}
+            placeholder="Select a country..."
+          />
         </div>
         <LineChart />
       </div>
